d8356ab492d77db4ed4225ec7eb2286fb6d23916
Wayne Mak <makwayne@gmail.com>
2011-08-17 21:51:25 -0700

    create our user model once the user authenticates with facebook

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,8 +1,13 @@
 class AuthenticationsController < ApplicationController
   def create
     user = User.new
-    # user.auth_key = request.env["omniauth.auth"][
-    render :json => request.env["omniauth.auth"]
+    omniauth = request.env["omniauth.auth"] 
+    user.auth_key = omniauth["credentials"]["token"]
+    user.name = omniauth["user_info"]["nickname"]
+    user.fb_id = omniauth["user_info"]["extra"]["user_hash"]["id"]
+    user.save
+
+    render :json => user
   end
 
 end
