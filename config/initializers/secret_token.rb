f9f827a9793144fad981715320a0fdeb7eaf2c81
Wayne Mak <makwayne@gmail.com>
2011-08-17 20:08:00 -0700

    first brogramming commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brogramming::Application.config.secret_token = '6ad48f4ef62249cc7df078885dddbd554d910ce2275a9cc359b723ffcff5f49058731d6dfa9c46fa69311368da4c3449d06202142325757370287c9e31787cc0'
