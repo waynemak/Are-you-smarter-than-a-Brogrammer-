d307ec071e92ac11d372f59a0635c3a249a2e001
Ryan Weald <ryan@weald.com>
2011-08-18 09:01:54 -0700

    make the questions actually redirect to the next question. Page is not the right parameter since we are using the show action. What we need to do instead is increment the :id field. In the future we should probably revise this to use the index action and page instead.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,11 +3,11 @@ class AnswersController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
 
-    return redirect_to question_path(@question, :page => @question.id + 1) unless params[:answer_options]
+    return redirect_to question_path(@question) unless params[:answer_options]
     if @question.correct_answer?(params[:answer_options])
       logger.info { "Update the answer count" }
     end
-    redirect_to question_path(@question, :page => @question.id + 1)
+    redirect_to question_path(:id => @question.id + 1)
   end
 
 end
