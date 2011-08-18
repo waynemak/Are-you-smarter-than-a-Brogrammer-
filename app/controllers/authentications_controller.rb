7af696da798f8df944ce3a664fcfae61dd07d113
Wayne Mak <makwayne@gmail.com>
2011-08-17 22:28:46 -0700

    refactor user creation into User.sign_in

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,13 +1,6 @@
 class AuthenticationsController < ApplicationController
   def create
-    user = User.new
-    omniauth = request.env["omniauth.auth"] 
-    user.auth_key = omniauth["credentials"]["token"]
-    user.name = omniauth["user_info"]["nickname"]
-    user.fb_id = omniauth["extra"]["user_hash"]["id"]
-    user.save
-
-    render :json => user
+    @user = User.sign_in(request.env["omniauth.auth"])
+    render :json => @user
   end
-
 end
