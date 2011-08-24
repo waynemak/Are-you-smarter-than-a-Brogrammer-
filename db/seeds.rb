5444b00c5837d2af98af6cd45f705c974e0b04b1
Wayne Mak <makwayne@gmail.com>
2011-08-23 22:10:33 -0700

    add questions to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,60 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+Question.create!(
+  :content => "Who is DHH, the patron saint of brogramming", 
+  :answers => ['David H. Hasselhoff', 'David Heinemeier Hansson'], 
+  :correct_answer => 1
+)
+
+Question.create!(
+  :content => "What's a brogrammer's favorite element", 
+  :answers => ['Boron', 'Krypton', 'Bromide', 'Yourmomium'], 
+  :correct_answer => 2
+)
+
+Question.create!(
+  :content => "What is BST", 
+  :answers => ['Binary Search Tree', 'Bull Shit'], 
+  :correct_answer => 0
+)
+
+Question.create!(
+  :content => "What is a Brogrammer's favorite beer", 
+  :answers => ['Amstel light', 'Budlight', 'Natty light'], 
+  :correct_answer => 2
+)
+
+
+Question.create!(
+  :content => "Which neighborhood has the highest BSI", 
+  :answers => ['Mission', 'Chinatown'], 
+  :correct_answer => 1
+)
+
+Question.create!(
+  :content => "When should raybans be worn", 
+  :answers => ['Outside on a sunny day', 'All the time'], 
+  :correct_answer => 1
+)
+
+Question.create!(
+  :content => "What is this number: 1011", 
+  :answers => ['1011', '33', '11'], 
+  :correct_answer => 1
+)
+
+Question.create!(
+  :content => "What do you fear most in life", 
+  :answers => ['Heights', 'Carpal tunnel', 'Death'], 
+  :correct_answer => 1
+)
+
+Question.create!(
+  :content => "What is recursion", 
+  :answers => ['Inheritance', 'Abstraction', 'What is recursion?', 'Exception'], 
+  :correct_answer => 2
+)
+
+Question.create!(
+  :content => "What do brogrammers do?", 
+  :answers => ['Fix bugs', 'Drink beer', 'Rage', 'All of the above'], 
+  :correct_answer => 0
+)
