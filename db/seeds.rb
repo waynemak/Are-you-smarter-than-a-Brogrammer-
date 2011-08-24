c142ad716c0d2273da11e82c09e02aa02f26dd8d
Wayne Mak <makwayne@gmail.com>
2011-08-23 22:14:40 -0700

    modify seeds file to make it rake

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,60 +1,60 @@
 Question.create!(
   :content => "Who is DHH, the patron saint of brogramming", 
-  :answers => ['David H. Hasselhoff', 'David Heinemeier Hansson'], 
+  :answer_options => ['David H. Hasselhoff', 'David Heinemeier Hansson'], 
   :correct_answer => 1
 )
 
 Question.create!(
   :content => "What's a brogrammer's favorite element", 
-  :answers => ['Boron', 'Krypton', 'Bromide', 'Yourmomium'], 
+  :answer_options => ['Boron', 'Krypton', 'Bromide', 'Yourmomium'], 
   :correct_answer => 2
 )
 
 Question.create!(
   :content => "What is BST", 
-  :answers => ['Binary Search Tree', 'Bull Shit'], 
+  :answer_options => ['Binary Search Tree', 'Bull Shit'], 
   :correct_answer => 0
 )
 
 Question.create!(
   :content => "What is a Brogrammer's favorite beer", 
-  :answers => ['Amstel light', 'Budlight', 'Natty light'], 
+  :answer_options => ['Amstel light', 'Budlight', 'Natty light'], 
   :correct_answer => 2
 )
 
 
 Question.create!(
   :content => "Which neighborhood has the highest BSI", 
-  :answers => ['Mission', 'Chinatown'], 
+  :answer_options => ['Mission', 'Chinatown'], 
   :correct_answer => 1
 )
 
 Question.create!(
   :content => "When should raybans be worn", 
-  :answers => ['Outside on a sunny day', 'All the time'], 
+  :answer_options => ['Outside on a sunny day', 'All the time'], 
   :correct_answer => 1
 )
 
 Question.create!(
   :content => "What is this number: 1011", 
-  :answers => ['1011', '33', '11'], 
+  :answer_options => ['1011', '33', '11'], 
   :correct_answer => 1
 )
 
 Question.create!(
   :content => "What do you fear most in life", 
-  :answers => ['Heights', 'Carpal tunnel', 'Death'], 
+  :answer_options => ['Heights', 'Carpal tunnel', 'Death'], 
   :correct_answer => 1
 )
 
 Question.create!(
   :content => "What is recursion", 
-  :answers => ['Inheritance', 'Abstraction', 'What is recursion?', 'Exception'], 
+  :answer_options => ['Inheritance', 'Abstraction', 'What is recursion?', 'Exception'], 
   :correct_answer => 2
 )
 
 Question.create!(
   :content => "What do brogrammers do?", 
-  :answers => ['Fix bugs', 'Drink beer', 'Rage', 'All of the above'], 
+  :answer_options => ['Fix bugs', 'Drink beer', 'Rage', 'All of the above'], 
   :correct_answer => 0
 )
