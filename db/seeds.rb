f46f0999ea59502558adcdf40f7c687ac383a469
Ryan Weald <ryan@weald.com>
2011-08-23 22:37:19 -0700

    Updated questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,54 +1,53 @@
 Question.create!(
-  :content => "Who is DHH, the patron saint of brogramming", 
+  :content => "Who is DHH, the patron saint of brogramming?", 
   :answer_options => ['David H. Hasselhoff', 'David Heinemeier Hansson'], 
   :correct_answer => 1
 )
 
 Question.create!(
-  :content => "What's a brogrammer's favorite element", 
+  :content => "What's a brogrammer's favorite element?", 
   :answer_options => ['Boron', 'Krypton', 'Bromide', 'Yourmomium'], 
   :correct_answer => 2
 )
 
 Question.create!(
-  :content => "What is BST", 
+  :content => "What is BST?", 
   :answer_options => ['Binary Search Tree', 'Bull Shit'], 
   :correct_answer => 0
 )
 
 Question.create!(
-  :content => "What is a brogrammer's favorite beer", 
+  :content => "What is a brogrammer's favorite beer?", 
   :answer_options => ['Amstel light', 'Budlight', 'Natty light'], 
   :correct_answer => 2
 )
 
-
 Question.create!(
-  :content => "Which neighborhood has the highest BSI", 
+  :content => "Which neighborhood has the highest BSI?", 
   :answer_options => ['Mission', 'Chinatown'], 
   :correct_answer => 1
 )
 
 Question.create!(
-  :content => "When should Ray-Bans be worn", 
+  :content => "When should Ray-Bans be worn?", 
   :answer_options => ['Outside on a sunny day', 'All the time'], 
   :correct_answer => 1
 )
 
 Question.create!(
-  :content => "What is this number: 1011", 
+  :content => "What is this number: 1011?", 
   :answer_options => ['1011', '33', '11'], 
   :correct_answer => 1
 )
 
 Question.create!(
-  :content => "What do you fear most in life", 
+  :content => "What do you fear most in life?", 
   :answer_options => ['Heights', 'Carpal tunnel', 'Death'], 
   :correct_answer => 1
 )
 
 Question.create!(
-  :content => "What is recursion", 
+  :content => "What is recursion?", 
   :answer_options => ['Inheritance', 'Abstraction', 'What is recursion?', 'Exception'], 
   :correct_answer => 2
 )
