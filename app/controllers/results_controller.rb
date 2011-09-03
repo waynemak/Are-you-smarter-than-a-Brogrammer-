db0b810db906a42a6f09b2e2362648cebe2810ca
Ryan Weald <ryan@weald.com>
2011-09-03 10:50:34 -0700

    add basic code and markup for leaderboard. Now it just needs some styling

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,8 +1,9 @@
 class ResultsController < ApplicationController
-  before_filter :authenticate_user
+  # before_filter :authenticate_user
   
   def show 
     @final_score = current_user.score
+    @leaders = User.order("score DESC").limit(10)
     @user = current_user
   end
 end
