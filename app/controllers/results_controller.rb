e8480a9772407232d4c3087ab737e13dda81101a
Ryan Weald <ryan@weald.com>
2011-09-04 12:55:24 -0700

    add some really basic styling for the leader board. Also refactor ResultsController a bit to remove duplication

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,9 +1,8 @@
 class ResultsController < ApplicationController
   # before_filter :authenticate_user
-  
+
   def show 
-    @final_score = current_user.score
     @leaders = User.order("score DESC").limit(10)
-    @user = current_user
+    @user = User.first
   end
 end
