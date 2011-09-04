536b0ddca119df62e32c9022f337d65f5b0f1ee9
Wayne Mak <makwayne@gmail.com>
2011-09-04 11:31:28 -0700

    make name a non-required field so that users without facebook names will still make it through the app flow without getting an error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   MAX_SCORE = 1
-  validates_presence_of :fb_id, :name
+  validates_presence_of :fb_id
 
   def self.sign_in(credentials)
     if (user = User.where(:fb_id => credentials["extra"]["user_hash"]["id"]).first)
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
     else
       user = User.create!(
         :auth_key   => credentials["credentials"]["token"],
-        :name       => credentials["user_info"]["nickname"].to_s,
+        :name       => credentials["user_info"]["nickname"],
         :fb_id      => credentials["extra"]["user_hash"]["id"]
       )
     end
